99220fc455459a8825f303e692c4b91392ccc519
Cristian <cristian.pozo@gmail.com>
2023-11-12 18:06:02 -0300

    Se añade opcion 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,33 @@
-let cantidad = document.querySelector('#cantidad')
-let precioInicial = document.querySelector('#precioInicial')
-let total = document.querySelector('#total')
-let mas = document.querySelector('#mas')
-let menos = document.querySelector('#menos')
+let cantidad = document.querySelector("#cantidad");
+let precioInicial = document.querySelector("#precioInicial");
+let total = document.querySelector("#total");
+let mas = document.querySelector("#mas");
+let menos = document.querySelector("#menos");
 
 mas.onclick = function () {
-    cantidad.innerHTML++;
-    total.innerHTML = (cantidad.innerHTML * precioInicial.innerHTML).toLocaleString('es-CL');
+  cantidad.innerHTML++; //cantidad.innerHTML = cantidad + 1;
+    total.innerHTML = (
+    cantidad.innerHTML * precioInicial.innerHTML
+    ).toLocaleString("es-CL");
 };
 
 menos.onclick = function () {
     cantidad.innerHTML--;
-    total.innerHTML = (cantidad.innerHTML * precioInicial.innerHTML).toLocaleString('es-CL');
+    total.innerHTML = (
+    cantidad.innerHTML * precioInicial.innerHTML
+    ).toLocaleString("es-CL");
 };
+
+// Opción 2
+
+// menos.addEventListener('click', function () {
+//     cantidad.innerHTML--;
+//     total.innerHTML =
+//     cantidad.innerHTML * precioInicial.innerHTML.toLocaleString();
+// });
+
+// mas.addEventListener('click', function () {
+//     cantidad.innerHTML++;
+//     total.innerHTML =
+//     cantidad.innerHTML * precioInicial.innerHTML.toLocaleString();
+// });
